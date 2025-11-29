ceb83117527690d8ab4b1c3eb9716ffe27672423
Ada Love <113861258+Adathelove@users.noreply.github.com>
2025-11-29 15:40:25 -0700

    test(test): basic test setup for daydir/validation

---
+++ conftest.py
@@ -0,0 +1,9 @@
+# conftest.py
+import sys
+import os
+
+# Ensure project root is on sys.path
+ROOT = os.path.abspath(os.path.dirname(__file__))
+if ROOT not in sys.path:
+    sys.path.insert(0, ROOT)
+
